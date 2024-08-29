0a381cb301ba6eff7304d8c9e3a92b9af5484e58
JJackson <112931780+JJacking@users.noreply.github.com>
2024-08-29 17:31:11 +0900

    base_setting
    update

---
+++ psproject/src/main/java/com/example/psproject/PsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.psproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsprojectApplication.class, args);
+	}
+
+}
